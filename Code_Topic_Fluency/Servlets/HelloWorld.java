f8e75950e19fbbaf9f645805f53b3619400d7e8a
james-gab <james-gab@users.noreply.github.com>
2016-11-12 23:24:19 -0600

    Add files via upload

---
+++ Code_Topic_Fluency/Servlets/HelloWorld.java
@@ -0,0 +1,42 @@
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class HelloWorld extends HttpServlet {
+ 
+  private String message;
+
+    /**
+     *
+     * @throws ServletException
+     */
+  @Override
+    public void init() throws ServletException
+  {
+      // Do required initialization
+      //message = getWeb.main();
+      message = "    Hello World, this is a test, and it worked!!!!";
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException
+  {
+      // Set response content type
+      response.setContentType("text/html");
+
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<h1>"+message+"<h1>");
+  }
+  
+  @Override
+  public void destroy()
+  {
+      // do nothing.
+  }
+
+}
